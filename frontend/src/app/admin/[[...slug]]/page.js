198f34dbe1ce00900c8115504925cb3b2f19f810
nhandzai <ducnhan1522004@gmail.com>
2025-04-18 23:23:49 +0700

    add tool / edit tool button

--- frontend/src/app/admin/[[...slug]]/page.js
+++ frontend/src/app/admin/[[...slug]]/page.js
@@ -1,5 +1,6 @@
 'use client';
 
+import { useState } from 'react';
 import Table from "@/component/Table";
 import PagingBar from "@/component/PagingBar";
 import EditTool from "@/component/EditTool";
@@ -26,7 +27,7 @@ const apiData = {
             toolDescription: 'Description for Tool 1',
             icon: '🔧',
             isPremium: false,
-            actions: {edit: 'put/tool/1', delete: 'delete/tool/1' },
+            actions: { edit: 'put/tool/1', delete: 'delete/tool/1' },
         },
         {
             id: 2,
@@ -42,21 +43,46 @@ const apiData = {
     pageSize: 2,
 };
 
-
-
 export default function ToolsPage() {
+    const [isEditToolOpen, setIsEditToolOpen] = useState(false);
+
+    const handleEditTool = (tool) => {
+       alert(`Tool edited: ${tool}`);
+    }
+
     return (
-        <div className="w-full">
+        <div className="w-full flex flex-col">
+            <button className="self-end cursor-pointer bg-yellow-400 text-white px-4 py-2 rounded-lg my-2 hover:bg-blue-600 transition duration-200"
+                onClick={() => setIsEditToolOpen(true)}>
+
+                Add Tool
+            </button>
             <Table columns={columns} data={apiData.data} />
-            <PagingBar 
-            total={apiData.total} 
-            page={apiData.page}
-            pageSize={apiData.pageSize}
-            onPageChange={handlePageChange}
+            <PagingBar
+                total={apiData.total}
+                page={apiData.page}
+                pageSize={apiData.pageSize}
+                onPageChange={handlePageChange}
             />
-{/*  
-            <EditTool toolName={'abc'} toolDescription={'lol'} icon={'icon1.png'}isPremium={false}/>
-*/}
+            <div>
+
+            </div>
+
+            {
+                isEditToolOpen && (
+                    
+                        <div className="absolute w-80 z-10 self-center">
+                            <EditTool
+                                toolName={'abc'}
+                                toolDescription={'lol'}
+                                icon={'icon1.png'}
+                                isPremium={false}
+                                onClose={() => setIsEditToolOpen(false)}
+                                onSave={handleEditTool}
+                            />
+                        </div>
+
+                )}
         </div>
     );
 }
